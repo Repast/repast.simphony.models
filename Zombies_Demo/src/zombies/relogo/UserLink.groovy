0588a7db545dc5e97d0a0f5f35429bfbf5860c89
Nick Collier <srcnick@sourceforge.net>
2012-01-13 14:21:50 -0500

    renamed Zombies to Zombies_Demo

---
+++ Zombies_Demo/src/zombies/relogo/UserLink.groovy
@@ -0,0 +1,15 @@
+package zombies.relogo
+
+import static repast.simphony.relogo.Utility.*;
+import static repast.simphony.relogo.UtilityG.*;
+import repast.simphony.relogo.BaseLink;
+import repast.simphony.relogo.Directed;
+import repast.simphony.relogo.Plural;
+import repast.simphony.relogo.Stop;
+import repast.simphony.relogo.Undirected;
+import repast.simphony.relogo.Utility;
+import repast.simphony.relogo.UtilityG;
+
+class UserLink extends BaseLink	{
+
+}

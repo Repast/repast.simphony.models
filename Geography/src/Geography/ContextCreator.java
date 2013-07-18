a7b19cd64a036a39ada31759d64152260490788c
Jonathan Ozik <xio247@gmail.com>
2013-07-18 12:02:48 -0500

    Merge commit.

--- Geography/src/Geography/ContextCreator.java
+++ Geography/src/Geography/ContextCreator.java
@@ -1,58 +1,136 @@
 package Geography;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Point;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
 import repast.simphony.context.Context;
 import repast.simphony.context.space.gis.GeographyFactoryFinder;
-import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.space.gis.Geography;
 import repast.simphony.space.gis.GeographyParameters;
-import repast.simphony.space.grid.GridBuilderParameters;
-import repast.simphony.space.grid.RandomGridAdder;
-import repast.simphony.space.grid.StickyBorders;
+import repast.simphony.visualization.gis3D.WWUtils;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
-public class ContextCreator implements ContextBuilder<GisAgent> {
+/**
+ * Model for testing NASA WorldWind display of geography projection
+ * 
+ * @author Eric Tatara
+ *
+ */
+public class ContextCreator implements ContextBuilder {
 
+	public Context build(Context context) {
 
-  public Context build(Context<GisAgent> context) {
+		GeographyParameters geoParams = new GeographyParameters();
+		Geography geography = GeographyFactoryFinder.createGeographyFactory(null)
+		.createGeography("Geography", context, geoParams);
 
-    GeographyParameters geoParams = new GeographyParameters();
-    Geography geography = GeographyFactoryFinder.createGeographyFactory(null)
-            .createGeography("Geography", context, geoParams);
+		GeometryFactory fac = new GeometryFactory();
 
-    GridFactoryFinder.createGridFactory(null).createGrid("Grid", context,
-            GridBuilderParameters.multiOccupancy2D(new RandomGridAdder(), new StickyBorders(),
-                    30, 30));
+		for (int i = 0; i < 1000; i++) {
+			GisAgent agent = new GisAgent("Site " + i);
+			context.add(agent);
+			Coordinate coord = new Coordinate(-87.75 + 0.1* Math.random(), 41.82 + 0.1 * Math.random());
+			Point geom = fac.createPoint(coord);
+			geography.move(agent, geom);
+		}
 
-    /*
-    File file = new File("/Users/nick/tmp/another.shp");
-    ShapefileLoader loader = null;
-    try {
-      loader = new ShapefileLoader(GisAgent.class, file.toURL(), geography, context);
-      loader.load();
-    } catch (MalformedURLException e) {
-      e.printStackTrace(); 
-    }
-    */
+    loadFeatures( "gisdata/Zones.shp", context, geography);
+//    loadFeatures( "gisdata/Agents.shp", context, geography);
+//    loadFeatures( "gisdata/Water_lines.shp", context, geography);
+	
+		return context;
+	}
+	
+	private void loadFeatures (String filename, Context context, Geography geography){
+		URL url = null;
+		try {
+			url = new File(filename).toURL();
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		SimpleFeatureIterator fiter = null;
+		ShapefileDataStore store = null;
+		try {
+			store = new ShapefileDataStore(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 
+		CoordinateReferenceSystem crs = null;
+		try {
+			crs = store.getSchema().getCoordinateReferenceSystem();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+//		if (crs != null)
+//			geography.setCRS(crs);
+		
+		try {
+			fiter = store.getFeatureSource().getFeatures().features();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    GeometryFactory fac = new GeometryFactory();
-
-    for (int i = 0; i < 10; i++) {
-      GisAgent agent = new GisAgent();
-      context.add(agent);
-      agent.setWealth(i);
-      Coordinate coord = new Coordinate(-103.823 + i / 100.0, 44.373);
-      Point geom = fac.createPoint(coord);
-      geography.move(agent, geom);
-    }
-
-
-
-    return context;
-  }
-
+		while(fiter.hasNext()){
+			SimpleFeature feature = fiter.next();
+      Geometry geom = (Geometry)feature.getDefaultGeometry();
+			Object agent = null;
+      
+			if (geom instanceof MultiPolygon){
+				MultiPolygon mp = (MultiPolygon)feature.getDefaultGeometry();
+				geom = (Polygon)mp.getGeometryN(0);
+				
+				String name = (String)feature.getAttribute("Name");
+				
+				agent = new ZoneAgent(name);
+			}
+			else if (geom instanceof Point){
+				geom = (Point)feature.getDefaultGeometry();				
+				String name = (String)feature.getAttribute("Name");
+//				double wealth = (double)feature.getAttribute("Wealth");
+				
+				agent = new GisAgent(name);
+			}
+			else if (geom instanceof MultiLineString){
+				MultiLineString line = (MultiLineString)feature.getDefaultGeometry();
+				geom = (LineString)line.getGeometryN(0);
+				String name = (String)feature.getAttribute("Name");
+				
+				agent = new WaterLine(name);
+			}
+			
+			if (agent != null){
+			  context.add(agent);
+			  
+			  if (crs != null){
+					WWUtils.projectGeometryToWGS84(geom, crs);
+				}
+			  
+			  geography.move(agent, geom);
+			}
+			else{
+				System.out.println("NULL agent for  " + geom);
+			}
+			
+		}				
+	}
 }
---
+++ Geography/src/Geography/WaterLine.java
@@ -0,0 +1,20 @@
+package Geography;
+
+public class WaterLine {
+
+	private String name;
+	
+	public WaterLine(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}

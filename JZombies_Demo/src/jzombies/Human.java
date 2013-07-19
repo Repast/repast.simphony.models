bfa4604678477e29bf0dd87a1c540a6369817a3b
Nick Collier <srcnick@sourceforge.net>
2013-07-19 17:19:39 -0400

    removed references to statecharts

--- JZombies_Demo/src/jzombies/Human.java
+++ JZombies_Demo/src/jzombies/Human.java
@@ -16,8 +16,6 @@ import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridPoint;
 import repast.simphony.util.SimUtilities;
-import jzombies.chart.DiseaseStatechart;
-import repast.simphony.ui.probe.ProbedProperty;
 
 /**
  * @author nick
@@ -28,8 +26,6 @@ public class Human {
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	private int energy, startingEnergy;
-	@ProbedProperty(displayName="Disease Statechart")
-	DiseaseStatechart diseaseStatechart = DiseaseStatechart.createStateChart(this, 0);
 
 	public Human(ContinuousSpace<Object> space, Grid<Object> grid, int energy) {
 		this.space = space;
